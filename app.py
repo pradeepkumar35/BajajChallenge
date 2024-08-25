249a811fded3e8f4fe8b493e17e05562b011736a
pradeepkumar35 <manojpradeep91@gmail.com>
2024-08-25 13:09:35 +0530

    first commit

--- app.py
+++ app.py
@@ -5,18 +5,15 @@ import json
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
-
 def extract_alphabets_and_numbers(data):
     alphabets = [char for char in data if isinstance(char, str) and char.isalpha()]
     numbers = [int(num) for num in data if isinstance(num, (int, str)) and str(num).isdigit()]
-    highest_lowercase_alphabet = max([char for char in alphabets if char.islower()], default = None)
+    highest_lowercase_alphabet = max([char for char in alphabets if char.islower()], default=None)
     return alphabets, numbers, highest_lowercase_alphabet
-
 
 @app.route('/bfhl', methods=['POST'])
 def bfhl():
     try:
-        # Parse the incoming JSON request
         request_data = request.get_json()
         if 'data' not in request_data:
             return jsonify({'error': 'Invalid JSON structure. "data" field is required.'}), 400
@@ -26,10 +23,8 @@ def bfhl():
         if not isinstance(data, list):
             return jsonify({'error': '"data" should be a list.'}), 400
 
-        # Process the data
         alphabets, numbers, highest_lowercase_alphabet = extract_alphabets_and_numbers(data)
 
-        # Prepare the response
         response = {
             'alphabets': alphabets,
             'numbers': numbers,
@@ -43,7 +38,6 @@ def bfhl():
 
     except Exception as e:
         return jsonify({'error': f'Server Error: {str(e)}'}), 500
-
 
 if __name__ == '__main__':
     app.run(debug=True)
